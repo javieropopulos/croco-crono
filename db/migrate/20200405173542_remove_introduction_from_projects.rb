e49381139c3a1d10fb0a82977cc58e72ef8934ef
xavier auffray <xavierauffray@hotmail.fr>
2020-04-08 16:56:27 +0200

    try to fix

--- db/migrate/20200405173542_remove_introduction_from_projects.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIntroductionFromProjects < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :projects, :introduction, :string
-  end
-end
--- db/migrate/20200405173558_remove_conclusion_from_projects.rb
+++
@@ -1,6 +0,0 @@
-class RemoveConclusionFromProjects < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :projects, :conclusion, :string
-  end
-end
