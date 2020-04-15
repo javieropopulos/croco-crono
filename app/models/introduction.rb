93d71b555a3ddc1076e01d20dc5f7adc645f9b93
xavier auffray <xavierauffray@hotmail.fr>
2020-04-15 09:43:58 +0200

    comment out order intro & conclusion

--- app/models/introduction.rb
+++ app/models/introduction.rb
@@ -1,5 +1,5 @@
 class Introduction < ApplicationRecord
   belongs_to :project
-  acts_as_list scope: :project
-  default_scope { order(position: :asc) }
+  # acts_as_list scope: :project
+  # default_scope { order(position: :asc) }
 end
